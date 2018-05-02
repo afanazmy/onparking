7af4ffb1697608d697a04b155f5ffb78f3fafd90
Muhammad Afan Azmi <afan123azmy@gmail.com>
2018-05-02 07:18:47 +0700

    Second Push

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Mahasiswa;
+use App\Transformers\MahasiswaTransformer;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -27,9 +29,31 @@ class AuthController extends Controller
             'prodi'     => $request->prodi
         ]);
 
-        return fractal()
-            ->item($mahasiswa['nama'])
+        $response = fractal()
+            ->item($mahasiswa)
             ->transformWith(new MahasiswaTransformer)
+            ->addMeta([
+                'token' => $mahasiswa->api_token,
+            ])
+            ->toArray();
+
+        return response()->json($response, 201);
+    }
+
+    public function login(Request $request, Mahasiswa $mahasiswa)
+    {
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json(['error' => 'Your credential is wrong'], 401);
+        }
+
+        $mahasiswa = $mahasiswa->find(Auth::user()->id);
+
+        return fractal()
+            ->item($mahasiswa)
+            ->transformWith(new MahasiswaTransformer)
+            ->addMeta([
+                'token' => $mahasiswa->api_token,
+            ])
             ->toArray();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('users', 'UserController@users');
 Route::get('mahasiswas', 'MahasiswaController@mahasiswas');
 Route::post('auth/register', 'AuthController@register');
+Route::post('auth/login', 'AuthController@login');
+Route::get('mahasiswas/profil', 'MahasiswaController@profil');
